93fe55d4eaa22e0fa14a5e3a28199e1e838b789e
Chineta Adinnu <chinetaadinnu@gmail.com>
2024-09-16 19:19:30 +0100

    update changes

--- src/controller/v1/users/auth.js
+++ src/controller/v1/users/auth.js
@@ -166,7 +166,6 @@ const login = async (req, res) => {
     await user.save();
     res.status(200).json({
       status: 200,
-      data: { email: user.email, name: user.first_name, id: user._id },
     });
   } catch (err) {
     res.status(500).json({ message: err.message });
